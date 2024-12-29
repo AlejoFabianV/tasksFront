04c41b118a93bf71c0e31193032bd16ecb83bc65
AlejoFabianV <verhagenalejo@gmail.com>
2024-12-29 18:12:17 -0300

    corregi los tipos

--- src/components/TaskForm.tsx
+++ src/components/TaskForm.tsx
@@ -8,7 +8,7 @@ function TaskForm() {
         description: "",
         completed: false,
     });
-    const [filtered, setFilter] = useState([]);
+    const [filtered, setFilter] = useState<typeof tasks>([]);
 
     const { createTask, tasks } = useTasks()
 
@@ -43,7 +43,7 @@ function TaskForm() {
             setFilter([]);
             return tasks;
         }
-        setFilter(filter);
+        setFilter(filter || []);
     }
 
     return (
@@ -78,7 +78,7 @@ function TaskForm() {
 
                     <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select an option</label>
                     <select 
-                        className=" mb-3 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        className=" mb-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                         onChange={handleFilter}
                     >  
                         <option value='all tasks'>All</option>
