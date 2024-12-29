f87c86dd3b0686b5243f081266cbd8dd98993f1e
AlejoFabianV <verhagenalejo@gmail.com>
2024-12-29 20:53:08 -0300

    arregle el estilo de fondo

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,8 @@ import { TaskProvider } from "./context/TaskContext"
 function App() {
 
   return (
-    <div className="bg-zinc-900 max-h-[100%] text-white flex items-center justify-center">
-      <div className="bg-gray-950 p-4 my-5 w-[800px] max-w-[97%] rounded-lg">
+    <div className="bg-zinc-900 grow min-h-screen text-white flex items-center justify-center">
+      <div className="bg-gray-950 p-4 w-[800px] max-w-[97%] rounded-lg">
         <h1 className="text-3xl font-bold text-center block my-2">Task Manager</h1>
 
         <TaskProvider>
