ae9090402c742da3e2ccb75badd744e074526b22
AlejoFabianV <verhagenalejo@gmail.com>
2024-12-29 20:54:35 -0300

    margenes en eje y

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <div className="bg-zinc-900 grow min-h-screen text-white flex items-center justify-center">
-      <div className="bg-gray-950 p-4 w-[800px] max-w-[97%] rounded-lg">
+      <div className="bg-gray-950 p-4 my-6 w-[800px] max-w-[97%] rounded-lg">
         <h1 className="text-3xl font-bold text-center block my-2">Task Manager</h1>
 
         <TaskProvider>
