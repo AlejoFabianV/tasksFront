ffb86977dad0b1b22670a7ad1d980fbf7a4cc8cc
AlejoFabianV <verhagenalejo@gmail.com>
2024-12-29 20:41:27 -0300

    estilos para el responsive de la app

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,8 @@ import { TaskProvider } from "./context/TaskContext"
 function App() {
 
   return (
-    <div className="bg-zinc-900 h-screen text-white flex items-center justify-center">
-      <div className="bg-gray-950 p-4 w-2/5">
+    <div className="bg-zinc-900 max-h-[100%] text-white flex items-center justify-center">
+      <div className="bg-gray-950 p-4 my-5 w-[800px] max-w-[90%] rounded-lg">
         <h1 className="text-3xl font-bold text-center block my-2">Task Manager</h1>
 
         <TaskProvider>
